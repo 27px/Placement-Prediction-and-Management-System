c51ec8e1fb56f651b5df79f1507710172257933b
27px <twentysevenpixels@gmail.com>
2020-10-25 13:31:42 +0530

    Styled 404 page

--- index.js
+++ index.js
@@ -12,23 +12,12 @@ var app=express();
 app.set("views","./views");
 app.set("view engine","ejs");
 
-// app.use("static",express.static(__dirname+"/static"));
-
-// app.use("/images",express.static(path.join(__dirname,"static","images")));
-
-// app.use("/style",express.static(path.join(__dirname,"static","style")));
-// app.use("/style/home",express.static(path.join(__dirname,"static","style","home")));
-// app.use("/script",express.static(path.join(__dirname,"static","script")));
-// app.use("/script/home",express.static(path.join(__dirname,"static","script","home")));
-// app.use("/plugins/chart.js",express.static(path.join(__dirname,"static","plugins","chart.js")));
-
-// app.use(favicon(path.join(__dirname,'static','images','favicon.ico')));
-
+app.use(favicon(path.join(__dirname,'static','images','favicon.ico')));
 
 app.use("/static",express.static(path.join(__dirname,"static")));
 
 app.use("/",route);
 
-app.listen(PORT,HOST,()=>{
+app.listen(PORT,()=>{
   console.log(`Started server ${HOST} on port : ${PORT}`);
 });
