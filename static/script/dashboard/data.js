7a8b59451021cd0f0bb0d75cb1d05595f9f4cc71
27px <twentysevenpixels@gmail.com>
2020-10-25 13:32:01 +0530

    Styled 404 page

---
+++ static/script/dashboard/data.js
@@ -0,0 +1,85 @@
+var app={
+  name:"Project Name",
+  description:"Project description in a single line here",
+  version:"1.2",
+  icon:"images/default-white.svg"
+}
+var options=[
+  {
+    title:"Dashboard",
+    subtitle:"dashboard",
+    icon:"images/default-white.svg",
+    action:"tabs/dashboard.txt"
+  },
+  {
+    title:"Profile",
+    subtitle:"profile",
+    icon:"images/default-white.svg",
+    action:"tabs/profile.txt"
+  },
+  {
+    title:"Option 1",
+    subtitle:"options",
+    icon:"images/default-white.svg",
+    action:"tabs/option1.txt"
+  },
+  {
+    title:"Option 2",
+    subtitle:"options",
+    icon:"images/default-white.svg",
+    action:"tabs/option2.txt"
+  },
+  {
+    title:"Option 3",
+    subtitle:"option",
+    icon:"images/default-white.svg",
+    action:"tabs/option3.txt"
+  },
+  {
+    title:"Option 4",
+    subtitle:"options",
+    icon:"images/default-white.svg",
+    action:"tabs/option4.txt"
+  },
+  {
+    title:"Option 5",
+    subtitle:"options",
+    icon:"images/default-white.svg",
+    action:"tabs/option5.txt"
+  },
+  {
+    title:"Option 6",
+    subtitle:"options",
+    icon:"images/default-white.svg",
+    action:"tabs/option6.txt"
+  },
+  {
+    title:"Settings",
+    subtitle:"settings",
+    icon:"images/default-white.svg",
+    action:""
+  }
+];
+var searchItems={
+  option:{
+    min:1//minimum number of characters required to start search
+  },
+  items:[
+    {
+      key:"home",
+      url:"index.html"
+    },
+    {
+      key:"settings",
+      url:"../new/settings.html"
+    },
+    {
+      key:"header",
+      url:"../new/hdr.html"
+    },
+    {
+      key:"help",
+      url:"../new/help.html"
+    }
+  ]
+};
