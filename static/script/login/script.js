875b3641533a6d75ab4bc66d6e30609dddfce80e
27px <twentysevenpixels@gmail.com>
2020-11-04 12:36:17 +0530

    Login-signup responsive design

--- static/script/login/script.js
+++ static/script/login/script.js
@@ -3,6 +3,7 @@ const hiddenPassword=`<svg xmlns="http://www.w3.org/2000/svg" width="44" height=
 const visiblePassword=`<svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye-off"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="3" y1="3" x2="21" y2="21"></line></svg>`;
 const _=key=>document.querySelector(key);
 const $=key=>document.querySelectorAll(key);
+const validateEmail=email=>/\S+@\S+\.\S+/.test(email);
 window.onload=()=>{
   body=document.body;
   _("#nav-login").addEventListener("click",function(){
@@ -19,6 +20,9 @@ window.onload=()=>{
   loginEmail=_("#login-email");
   loginPassword=_("#login-password");
   loginRemember=_("#login-remember");
+  registerEmail=_("#register-email");
+  registerPassword=_("#register-password");
+  registerConfirmPassword=_("#register-confirm-password");
 
   Array.from($(".togglePassword")).forEach(tbutton=>{
     tbutton.addEventListener("click",function(){
