4d3c6bc8ff0fe5a63677bece749650121907136f
27px <twentysevenpixels@gmail.com>
2020-11-05 21:20:56 +0530

    added loading animation on login-signup button

--- functions/user.js
+++ functions/user.js
@@ -1,22 +1,54 @@
+const userDefaultCodes={
+  "guest":0,
+  "student":1,
+  "coordinator":2,
+  "recruiter":3,
+  "admin":4
+};
 class User
 {
-  constructor()
+  constructor(session)
   {
-    this.isLoggedIn=false;//change
-    //loggedin code
-    this.type="guest";//change
-    this.userDefaultCodes={
-      "guest":0,
-      "student":1,
-      "coordinator":2,
-      "recruiter":3,
-      "admin":4
-    };
-    this.code=this.userDefaultCodes[this.type];
+    this.isLoggedIn=false;//default
+    this.type="guest";//default
+    if(session.user!==undefined && session.type!==undefined)
+    {
+      this.isLoggedIn=true;
+      this.type=session.type;
+      this.user=session.user;
+    }
+    this.code=userDefaultCodes[this.type];
   }
-  userCode(user="guest")
+  userCode()
   {
-    return this.userDefaultCodes[user];
+    return userDefaultCodes[this.type];
+  }
+  hasAccessOf(access)//type other user types
+  {
+    if(access=="guest")//All users have access previlage of guest
+    {
+      return true;
+    }
+    else if(this.type=="student" && access=="student")
+    {
+      return true;
+    }
+    else if(this.type=="coordinator" && ["student","coordinator"].includes(access))//coordinator is also a student
+    {
+      return true;
+    }
+    else if(this.type=="recruiter" && access=="recruiter")//recruiter has no other previlages.
+    {
+      return true;
+    }
+    else if(this.type=="admin" && access=="admin")//Admin have coordinator access too
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
 }
 module.exports=User;
--- routes/student.js
+++ routes/student.js
@@ -3,7 +3,6 @@ const express=require("express");
 const studentTabs=require("./student-tabs");
 const student=express.Router();
 const User=require("../functions/user.js");
-const user=new User();
 
 //Route Student
 student.get("/",(req,res)=>{
@@ -12,8 +11,9 @@ student.get("/",(req,res)=>{
 
 //Student Dashboard
 student.get("/dashboard",(req,res)=>{
+  const user=new User(req.session);
   //use user class for checking login
-  console.log(req.session.user,req.session.type);
+  // console.log(req.session.user,req.session.type);
   //student or coordinator
   if(req.session.user!==undefined && ( req.session.type==="student" || req.session.type==="coordinator" ))
   {
@@ -35,9 +35,9 @@ student.get("/dashboard",(req,res)=>{
 
 //Profile of others or current user
 student.get("/profile/:id",(req,res)=>{
-  var user=req.params.id;
+  var userID=req.params.id;
   res.render("student/view-profile",{
-    user
+    user:userID
   });
 });
 
