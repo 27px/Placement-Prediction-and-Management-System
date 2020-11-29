352b11c25db4642e255f36e7487099dc341ffbd3
GAM <glorinaaippu@gmail.com>
2020-11-29 11:30:23 +0530

    added about page,feedback form

--- config/mail.js
+++ config/mail.js
@@ -3,9 +3,14 @@ var mail_credentials={
   "service":user_config.MAIL.SERVICE,
   // "port":user_config.MAIL.PORT,
   // "secure":user_config.MAIL.SECURE,
+  secure:false, // use SSL
+  port:25, // smtp port
   "auth":{
     "user":user_config.MAIL.MAIL,
     "pass":user_config.MAIL.PASSWORD
+  },
+  tls:{
+    rejectUnauthorized: false
   }
 }
 module.exports=mail_credentials;
